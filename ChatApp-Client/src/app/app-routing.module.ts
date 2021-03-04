fe7fb897f46dd8430c04e46c5b3fccaa64494cec
Zukson <kapiolimpia@wp.pl>
2021-03-04 11:27:21 +0100

    add chat room component

--- ChatApp-Client/src/app/app-routing.module.ts
+++ ChatApp-Client/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { LoginComponent } from './login/login.component';
 import{RegisterComponent} from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import{MainComponent} from './main/main.component'
+import { ChatRoomComponent } from './chat-room/chat-room.component';
 const routes: Routes = [{path:'register',component:RegisterComponent},
 {path:'login',component:LoginComponent},{
   path:'',component:LoginComponent},
-  {path:'main',component:MainComponent,children:[{path:'profile',component:ProfileComponent},{path:'',component:ProfileComponent}]}
+  {path:'main',component:MainComponent,children:[{path:'profile',component:ProfileComponent},{path:'',component:ProfileComponent},  {path:'chatroom',component:ChatRoomComponent} ]},
+ 
 
 ];
 
--- ChatApp-Client/src/app/chat-room/chat-room.component.ts
+++ ChatApp-Client/src/app/chat-room/chat-room.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageModel } from '../models/message-model';
 
 @Component({
   selector: 'app-chat-room',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatRoomComponent implements OnInit {
 
+  messages:MessageModel[] = [];
+ chatroomId:string
   constructor() { }
 
   ngOnInit(): void {
